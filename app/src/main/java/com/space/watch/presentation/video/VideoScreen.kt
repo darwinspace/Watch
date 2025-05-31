89041b6b06e146cf3ecfc78d677ad9a34b9754d7
Darwin Delgado <darwindp@outlook.com>
2025-05-31 04:23:54 -0700

    Updated Dependencies
    This commit updates various dependencies to their latest versions, including:
    - Android Gradle Plugin to 8.10.1
    - Firebase BOM to 33.14.0
    - Kotlin to 2.1.20
    - Core KTX to 1.16.0
    - Lifecycle Runtime KTX to 2.9.0
    - Activity Compose to 1.10.1
    - Compose BOM to 2025.05.01
    - Material Icons Extended to 1.7.8
    - Media3 UI to 1.7.1
    - Navigation Compose to 2.9.0
    - Gradle to 8.11.1
    
    Additionally, it adds the `androidx-media3-ui-compose` dependency and updates project configuration files. The `VideoContent` composable in `VideoScreen.kt` has been refactored to remove the `@OptIn(UnstableApi::class)` annotation.

--- app/src/main/java/com/space/watch/presentation/video/VideoScreen.kt
+++ app/src/main/java/com/space/watch/presentation/video/VideoScreen.kt
@@ -1,6 +1,5 @@
 package com.space.watch.presentation.video
 
-import androidx.annotation.OptIn
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
@@ -52,7 +51,6 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.media3.common.MediaItem
-import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import coil.compose.AsyncImage
@@ -66,285 +64,282 @@ import com.space.watch.presentation.`interface`.theme.WatchTheme
 @Preview
 @Composable
 fun VideoScreenPreview() {
-    WatchTheme {
-        VideoScreen(
-            state = VideoScreenState.Content(
-                videoInformation = VideoInformation(
-                    id = String(),
-                    title = "Video",
-                    description = "Description",
-                    content = String(),
-                    size = Size(1280, 720),
-                    image = String(),
-                    imageSize = Size(),
-                    creator = Creator(
-                        id = String(),
-                        name = "Creator",
-                        description = String(),
-                        image = String(),
-                        cover = String(),
-                        verified = true
-                    ),
-                    duration = VideoDuration(0, 0, 0)
-                )
-            )
-        )
-    }
+	WatchTheme {
+		VideoScreen(
+			state = VideoScreenState.Content(
+				videoInformation = VideoInformation(
+					id = String(),
+					title = "Video",
+					description = "Description",
+					content = String(),
+					size = Size(1280, 720),
+					image = String(),
+					imageSize = Size(),
+					creator = Creator(
+						id = String(),
+						name = "Creator",
+						description = String(),
+						image = String(),
+						cover = String(),
+						verified = true
+					),
+					duration = VideoDuration(0, 0, 0)
+				)
+			)
+		)
+	}
 }
 
 @Composable
 fun VideoScreen(
-    state: VideoScreenState,
-    onBackButtonClick: () -> Unit = { },
-    onVideoCreatorClick: (String) -> Unit = { }
+	state: VideoScreenState,
+	onBackButtonClick: () -> Unit = { },
+	onVideoCreatorClick: (String) -> Unit = { }
 ) {
-    Box {
-        Scaffold { padding ->
-            AnimatedContent(targetState = state, label = "VideoScreenAnimation") { state ->
-                when (state) {
-                    is VideoScreenState.Content -> {
-                        VideoScreenContent(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(padding),
-                            videoInformation = state.videoInformation,
-                            onVideoCreatorClick = onVideoCreatorClick
-                        )
-                    }
+	Box {
+		Scaffold { padding ->
+			AnimatedContent(targetState = state, label = "VideoScreenAnimation") { state ->
+				when (state) {
+					is VideoScreenState.Content -> {
+						VideoScreenContent(
+							modifier = Modifier
+								.fillMaxSize()
+								.padding(padding),
+							videoInformation = state.videoInformation,
+							onVideoCreatorClick = onVideoCreatorClick
+						)
+					}
 
-                    VideoScreenState.Wait -> {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(padding),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
-                    }
+					VideoScreenState.Wait -> {
+						Box(
+							modifier = Modifier
+								.fillMaxSize()
+								.padding(padding),
+							contentAlignment = Alignment.Center
+						) {
+							CircularProgressIndicator()
+						}
+					}
 
-                    VideoScreenState.Empty -> Unit
-                }
-            }
-        }
+					VideoScreenState.Empty -> Unit
+				}
+			}
+		}
 
-        OutlinedIconButton(
-            modifier = Modifier.padding(16.dp),
-            colors = IconButtonDefaults.outlinedIconButtonColors(
-                containerColor = Color.Black.copy(alpha = 0.2f),
-                contentColor = Color.White
-            ),
-            border = BorderStroke(
-                width = 2.dp,
-                color = Color.White.copy(alpha = 0.1f),
-            ),
-            onClick = onBackButtonClick
-        ) {
-            Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
-        }
-    }
+		OutlinedIconButton(
+			modifier = Modifier.padding(16.dp),
+			colors = IconButtonDefaults.outlinedIconButtonColors(
+				containerColor = Color.Black.copy(alpha = 0.2f),
+				contentColor = Color.White
+			),
+			border = BorderStroke(
+				width = 2.dp,
+				color = Color.White.copy(alpha = 0.1f),
+			),
+			onClick = onBackButtonClick
+		) {
+			Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+		}
+	}
 }
 
 @Composable
 private fun VideoScreenContent(
-    modifier: Modifier,
-    videoInformation: VideoInformation,
-    onVideoCreatorClick: (String) -> Unit
+	modifier: Modifier,
+	videoInformation: VideoInformation,
+	onVideoCreatorClick: (String) -> Unit
 ) {
-    LazyColumn(modifier = modifier) {
-        item {
-            Column {
-                VideoContent(videoInformation)
+	LazyColumn(modifier = modifier) {
+		item {
+			Column {
+				VideoContent(videoInformation)
 
-                VideoInformation(videoInformation, onVideoCreatorClick)
-            }
-        }
-    }
+				VideoInformation(videoInformation, onVideoCreatorClick)
+			}
+		}
+	}
 }
 
-@OptIn(UnstableApi::class)
 @Composable
 fun VideoContent(videoInformation: VideoInformation) {
-    val context = LocalContext.current
-    val exoPlayer = remember {
-        val mediaItem = MediaItem.fromUri(videoInformation.content)
-        ExoPlayer.Builder(context).build().apply {
-            setMediaItem(mediaItem)
-            prepare()
-        }
-    }
+	val context = LocalContext.current
+	val player = remember {
+		val mediaItem = MediaItem.fromUri(videoInformation.content)
+		ExoPlayer.Builder(context).build().apply {
+			setMediaItem(mediaItem)
+			prepare()
+		}
+	}
 
-    var lifecycleEvent by remember { mutableStateOf(Lifecycle.Event.ON_CREATE) }
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            lifecycleEvent = event
-        }
+	var lifecycleEvent by remember { mutableStateOf(Lifecycle.Event.ON_CREATE) }
+	val lifecycleOwner = LocalLifecycleOwner.current
+	DisposableEffect(lifecycleOwner) {
+		val observer = LifecycleEventObserver { _, event ->
+			lifecycleEvent = event
+		}
 
-        lifecycleOwner.lifecycle.addObserver(observer)
+		lifecycleOwner.lifecycle.addObserver(observer)
 
-        onDispose {
-            exoPlayer.release()
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    }
+		onDispose {
+			player.release()
+			lifecycleOwner.lifecycle.removeObserver(observer)
+		}
+	}
 
-    AndroidView(
-        modifier = Modifier
-            .aspectRatio(ratio = videoInformation.size.width.toFloat() / videoInformation.size.height.toFloat())
-            .fillMaxWidth(),
-        factory = {
-            PlayerView(it).apply {
-                player = exoPlayer
-                setShowRewindButton(false)
-                setShowFastForwardButton(false)
-            }
-        },
-        update = {
-            when (lifecycleEvent) {
-                Lifecycle.Event.ON_RESUME -> {
-                    it.onResume()
-                }
+	AndroidView(
+		modifier = Modifier
+			.aspectRatio(ratio = videoInformation.size.width.toFloat() / videoInformation.size.height.toFloat())
+			.fillMaxWidth(),
+		factory = {
+			PlayerView(it).apply {
+				this.player = player
+			}
+		},
+		update = {
+			when (lifecycleEvent) {
+				Lifecycle.Event.ON_RESUME -> {
+					it.onResume()
+				}
 
-                Lifecycle.Event.ON_PAUSE -> {
-                    it.onPause()
-                    it.player?.pause()
-                }
+				Lifecycle.Event.ON_PAUSE -> {
+					it.onPause()
+					it.player?.pause()
+				}
 
-                else -> Unit
-            }
-        }
-    )
+				else -> Unit
+			}
+		}
+	)
 }
 
 @Composable
 fun VideoInformation(videoInformation: VideoInformation, onVideoCreatorClick: (String) -> Unit) {
-    Column(modifier = Modifier.padding(12.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            VideoTitle(videoInformation.title)
+	Column(modifier = Modifier.padding(12.dp)) {
+		Row(verticalAlignment = Alignment.CenterVertically) {
+			VideoTitle(videoInformation.title)
 
-            VideoFavoriteButton()
-        }
+			VideoFavoriteButton()
+		}
 
-        VideoCreator(
-            creator = videoInformation.creator,
-            onClick = {
-                onVideoCreatorClick(videoInformation.creator.id)
-            }
-        )
+		VideoCreator(
+			creator = videoInformation.creator,
+			onClick = {
+				onVideoCreatorClick(videoInformation.creator.id)
+			}
+		)
 
-        VideoDescription(videoInformation.description)
-    }
+		VideoDescription(videoInformation.description)
+	}
 }
 
 @Composable
 private fun RowScope.VideoTitle(title: String) {
-    Text(
-        modifier = Modifier
-            .padding(horizontal = 12.dp)
-            .weight(1f),
-        text = title,
-        style = MaterialTheme.typography.bodyMedium
-    )
+	Text(
+		modifier = Modifier
+			.padding(horizontal = 12.dp)
+			.weight(1f),
+		text = title,
+		style = MaterialTheme.typography.bodyMedium
+	)
 }
 
 @Composable
 private fun VideoFavoriteButton() {
-    IconButton(onClick = { /*TODO*/ }) {
-        Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = null)
-    }
+	IconButton(onClick = { /*TODO*/ }) {
+		Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = null)
+	}
 }
 
 @Composable
 private fun VideoCreator(creator: Creator, onClick: () -> Unit) {
-    Surface(
-        shape = MaterialTheme.shapes.large,
-        onClick = onClick
-    ) {
-        Row(
-            modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            VideoCreatorImage(creator.image)
+	Surface(
+		shape = MaterialTheme.shapes.large,
+		onClick = onClick
+	) {
+		Row(
+			modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
+			horizontalArrangement = Arrangement.spacedBy(12.dp),
+			verticalAlignment = Alignment.CenterVertically
+		) {
+			VideoCreatorImage(creator.image)
 
-            VideoCreatorName(creator.name)
+			VideoCreatorName(creator.name)
 
-            if (creator.verified) {
-                VideoCreatorVerifiedIcon()
-            }
-        }
-    }
+			if (creator.verified) {
+				VideoCreatorVerifiedIcon()
+			}
+		}
+	}
 }
 
 @Composable
 private fun VideoCreatorImage(image: String?) {
-    Box(
-        modifier = Modifier
-            .clip(shape = CircleShape)
-            .border(
-                width = 2.dp,
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
-                shape = CircleShape
-            )
-    ) {
-        AsyncImage(
-            modifier = Modifier
-                .clip(shape = CircleShape)
-                .size(32.dp),
-            model = image,
-            contentDescription = null
-        )
-    }
+	Box(
+		modifier = Modifier
+			.clip(shape = CircleShape)
+			.border(
+				width = 2.dp,
+				color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
+				shape = CircleShape
+			)
+	) {
+		AsyncImage(
+			modifier = Modifier
+				.clip(shape = CircleShape)
+				.size(32.dp),
+			model = image,
+			contentDescription = null
+		)
+	}
 }
 
 @Composable
 private fun RowScope.VideoCreatorName(name: String) {
-    Text(
-        modifier = Modifier.weight(1f),
-        text = name,
-        style = TextStyle(
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-            fontFamily = FontFamily(Font(R.font.mono_medium)),
-            fontWeight = FontWeight(500)
-        ),
-        maxLines = 1,
-    )
+	Text(
+		modifier = Modifier.weight(1f),
+		text = name,
+		style = TextStyle(
+			fontSize = 16.sp,
+			lineHeight = 24.sp,
+			fontFamily = FontFamily(Font(R.font.mono_medium)),
+			fontWeight = FontWeight(500)
+		),
+		maxLines = 1,
+	)
 }
 
 @Composable
 fun VideoCreatorVerifiedIcon() {
-    Icon(
-        imageVector = Icons.Outlined.Verified,
-        contentDescription = null,
-        tint = MaterialTheme.colorScheme.primary
-    )
+	Icon(
+		imageVector = Icons.Outlined.Verified,
+		contentDescription = null,
+		tint = MaterialTheme.colorScheme.primary
+	)
 }
 
 @Composable
 private fun VideoDescription(description: String) {
-    if (description.isNotBlank()) {
-        Surface(
-            modifier = Modifier.padding(12.dp),
-            shape = MaterialTheme.shapes.small,
-            border = BorderStroke(
-                width = 2.dp,
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
-            )
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
-                text = description,
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    lineHeight = 24.sp,
-                    fontFamily = FontFamily(Font(R.font.mono_medium)),
-                    fontWeight = FontWeight(500)
-                )
-            )
-        }
-    }
+	if (description.isNotBlank()) {
+		Surface(
+			modifier = Modifier.padding(12.dp),
+			shape = MaterialTheme.shapes.small,
+			border = BorderStroke(
+				width = 2.dp,
+				color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
+			)
+		) {
+			Text(
+				modifier = Modifier
+					.padding(16.dp)
+					.fillMaxWidth(),
+				text = description,
+				style = TextStyle(
+					fontSize = 14.sp,
+					lineHeight = 24.sp,
+					fontFamily = FontFamily(Font(R.font.mono_medium)),
+					fontWeight = FontWeight(500)
+				)
+			)
+		}
+	}
 }
