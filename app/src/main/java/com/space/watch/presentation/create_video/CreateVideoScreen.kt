ef41e84b8a2dbfbc37db3c05299384cd8242c91c
Darwin Delgado <darwindp@outlook.com>
2025-05-31 13:48:55 -0700

    Code refactoring.

--- app/src/main/java/com/space/watch/presentation/create_video/CreateVideoScreen.kt
+++ app/src/main/java/com/space/watch/presentation/create_video/CreateVideoScreen.kt
@@ -40,11 +40,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -53,14 +49,9 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerView
 import androidx.media3.ui.compose.PlayerSurface
 import coil.compose.AsyncImage
 import com.space.watch.domain.model.Image
@@ -69,344 +60,345 @@ import com.space.watch.extension.toImage
 import com.space.watch.extension.toVideo
 import com.space.watch.presentation.core.theme.WatchTheme
 import com.space.watch.presentation.video.PlayPauseButton
-import com.space.watch.presentation.video.ratio
 
 @Preview
 @Composable
 fun CreateVideoScreenPreview() {
-    WatchTheme {
-        CreateVideoScreen()
-    }
+	WatchTheme {
+		CreateVideoScreen()
+	}
 }
 
 @Composable
 fun CreateVideoScreen(
-    videoTitle: () -> String = { String() },
-    onVideoTitleChange: (String) -> Unit = { },
-    videoDescription: () -> String = { String() },
-    onVideoDescriptionChange: (String) -> Unit = { },
-    video: () -> Video? = { null },
-    onVideoChange: (Video?) -> Unit = { },
-    videoImage: () -> Image? = { null },
-    onVideoImageChange: (Image?) -> Unit = { },
-    isCreateVideoButtonEnabled: () -> Boolean = { false },
-    isVideoUploading: () -> Boolean = { false },
-    onBackButtonClick: () -> Unit = { },
-    onCreateVideoClick: () -> Unit = { }
+	videoTitle: () -> String = { String() },
+	onVideoTitleChange: (String) -> Unit = { },
+	videoDescription: () -> String = { String() },
+	onVideoDescriptionChange: (String) -> Unit = { },
+	video: () -> Video? = { null },
+	onVideoChange: (Video?) -> Unit = { },
+	videoImage: () -> Image? = { null },
+	onVideoImageChange: (Image?) -> Unit = { },
+	isCreateVideoButtonEnabled: () -> Boolean = { false },
+	isVideoUploading: () -> Boolean = { false },
+	onBackButtonClick: () -> Unit = { },
+	onCreateVideoClick: () -> Unit = { }
 ) {
-    val scrollState = rememberScrollState()
-    val context = LocalContext.current
+	val scrollState = rememberScrollState()
+	val context = LocalContext.current
 
-    val videoPickerLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickVisualMedia(),
-        onResult = {
-            onVideoChange(it.toVideo(context))
-        }
-    )
+	val videoPickerLauncher = rememberLauncherForActivityResult(
+		contract = ActivityResultContracts.PickVisualMedia(),
+		onResult = {
+			val video = it.toVideo(context)
+			onVideoChange(video)
+		}
+	)
 
-    val imagePickerLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickVisualMedia(),
-        onResult = {
-            onVideoImageChange(it.toImage(context))
-        }
-    )
+	val imagePickerLauncher = rememberLauncherForActivityResult(
+		contract = ActivityResultContracts.PickVisualMedia(),
+		onResult = {
+			val image = it.toImage(context)
+			onVideoImageChange(image)
+		}
+	)
 
-    Scaffold(
-        topBar = {
-            CreateVideoScreenTopBar(
-                isCreateVideoButtonEnabled = isCreateVideoButtonEnabled,
-                isVideoUploading = isVideoUploading,
-                onBackButtonClick = onBackButtonClick,
-                onCreateVideoClick = onCreateVideoClick
-            )
-        }
-    ) { padding ->
-        Column(
-            modifier = Modifier
+	Scaffold(
+		topBar = {
+			CreateVideoScreenTopBar(
+				isCreateVideoButtonEnabled = isCreateVideoButtonEnabled,
+				isVideoUploading = isVideoUploading,
+				onBackButtonClick = onBackButtonClick,
+				onCreateVideoClick = onCreateVideoClick
+			)
+		}
+	) { padding ->
+		Column(
+			modifier = Modifier
                 .verticalScroll(scrollState)
                 .fillMaxSize()
                 .padding(24.dp)
                 .padding(padding),
-            verticalArrangement = Arrangement.spacedBy(24.dp)
-        ) {
-            VideoTitleTextField(
-                videoTitle = videoTitle,
-                onVideoTitleChange = onVideoTitleChange
-            )
+			verticalArrangement = Arrangement.spacedBy(24.dp)
+		) {
+			VideoTitleTextField(
+				videoTitle = videoTitle,
+				onVideoTitleChange = onVideoTitleChange
+			)
 
-            VideoDescriptionTextField(
-                videoDescription = videoDescription,
-                onVideoDescriptionChange = onVideoDescriptionChange
-            )
+			VideoDescriptionTextField(
+				videoDescription = videoDescription,
+				onVideoDescriptionChange = onVideoDescriptionChange
+			)
 
-            SelectVideoButton(
-                onClick = {
-                    val input = PickVisualMediaRequest(VideoOnly)
-                    videoPickerLauncher.launch(input)
-                }
-            )
+			SelectVideoButton(
+				onClick = {
+					val input = PickVisualMediaRequest(VideoOnly)
+					videoPickerLauncher.launch(input)
+				}
+			)
 
-            val videoData = video()
-            AnimatedVisibility(visible = videoData != null) {
-                SelectedVideo(
-                    video = videoData!!
-                )
-            }
+			val videoData = video()
+			AnimatedVisibility(visible = videoData != null) {
+				SelectedVideo(
+					video = videoData!!
+				)
+			}
 
-            SelectVideoImageButton(
-                onClick = {
-                    val input = PickVisualMediaRequest(ImageOnly)
-                    imagePickerLauncher.launch(input)
-                }
-            )
+			SelectVideoImageButton(
+				onClick = {
+					val input = PickVisualMediaRequest(ImageOnly)
+					imagePickerLauncher.launch(input)
+				}
+			)
 
-            val imageData = videoImage()
-            AnimatedVisibility(visible = imageData != null) {
-                SelectedVideoImage(
-                    image = imageData!!
-                )
-            }
-        }
-    }
+			val imageData = videoImage()
+			AnimatedVisibility(visible = imageData != null) {
+				SelectedVideoImage(
+					image = imageData!!
+				)
+			}
+		}
+	}
 }
 
 @Composable
 private fun CreateVideoScreenTopBar(
-    isCreateVideoButtonEnabled: () -> Boolean,
-    isVideoUploading: () -> Boolean,
-    onBackButtonClick: () -> Unit,
-    onCreateVideoClick: () -> Unit
+	isCreateVideoButtonEnabled: () -> Boolean,
+	isVideoUploading: () -> Boolean,
+	onBackButtonClick: () -> Unit,
+	onCreateVideoClick: () -> Unit
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Surface {
-            Row(
-                modifier = Modifier
+	Column(
+		horizontalAlignment = Alignment.CenterHorizontally,
+		verticalArrangement = Arrangement.Center
+	) {
+		Surface {
+			Row(
+				modifier = Modifier
                     .statusBarsPadding()
                     .fillMaxWidth()
                     .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                OutlinedIconButton(
-                    border = BorderStroke(
-                        width = 2.dp,
-                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-                    ),
-                    onClick = onBackButtonClick
-                ) {
-                    Icon(imageVector = Icons.Default.Close, contentDescription = null)
-                }
+				horizontalArrangement = Arrangement.SpaceBetween,
+				verticalAlignment = Alignment.CenterVertically
+			) {
+				OutlinedIconButton(
+					border = BorderStroke(
+						width = 2.dp,
+						color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+					),
+					onClick = onBackButtonClick
+				) {
+					Icon(imageVector = Icons.Default.Close, contentDescription = null)
+				}
 
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    AnimatedVisibility(
-                        visible = isVideoUploading()
-                    ) {
-                        CircularProgressIndicator(
-                            modifier = Modifier.size(40.dp)
-                        )
-                    }
+				Row(
+					horizontalArrangement = Arrangement.spacedBy(16.dp),
+					verticalAlignment = Alignment.CenterVertically
+				) {
+					AnimatedVisibility(
+						visible = isVideoUploading()
+					) {
+						CircularProgressIndicator(
+							modifier = Modifier.size(40.dp)
+						)
+					}
 
-                    Button(
-                        enabled = isCreateVideoButtonEnabled(),
-                        border = BorderStroke(
-                            width = 2.dp,
-                            color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.1f),
-                        ),
-                        onClick = onCreateVideoClick
-                    ) {
-                        Text(text = "Create")
-                    }
-                }
-            }
-        }
+					Button(
+						enabled = isCreateVideoButtonEnabled(),
+						border = BorderStroke(
+							width = 2.dp,
+							color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.1f),
+						),
+						onClick = onCreateVideoClick
+					) {
+						Text(text = "Create")
+					}
+				}
+			}
+		}
 
-        HorizontalDivider(
-            thickness = 2.dp,
-            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
-        )
-    }
+		HorizontalDivider(
+			thickness = 2.dp,
+			color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
+		)
+	}
 }
 
 @Composable
 private fun VideoTitleTextField(
-    videoTitle: () -> String,
-    onVideoTitleChange: (String) -> Unit
+	videoTitle: () -> String,
+	onVideoTitleChange: (String) -> Unit
 ) {
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = videoTitle(),
-        onValueChange = onVideoTitleChange,
-        textStyle = MaterialTheme.typography.bodySmall,
-        shape = MaterialTheme.shapes.small,
-        keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.Sentences,
-            imeAction = ImeAction.Next
-        ),
-        placeholder = {
-            Text(
-                text = "Title",
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
-    )
+	OutlinedTextField(
+		modifier = Modifier.fillMaxWidth(),
+		value = videoTitle(),
+		onValueChange = onVideoTitleChange,
+		textStyle = MaterialTheme.typography.bodySmall,
+		shape = MaterialTheme.shapes.small,
+		keyboardOptions = KeyboardOptions(
+			capitalization = KeyboardCapitalization.Sentences,
+			imeAction = ImeAction.Next
+		),
+		placeholder = {
+			Text(
+				text = "Title",
+				style = MaterialTheme.typography.bodySmall
+			)
+		}
+	)
 }
 
 @Composable
 private fun VideoDescriptionTextField(
-    videoDescription: () -> String,
-    onVideoDescriptionChange: (String) -> Unit
+	videoDescription: () -> String,
+	onVideoDescriptionChange: (String) -> Unit
 ) {
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = videoDescription(),
-        onValueChange = onVideoDescriptionChange,
-        textStyle = MaterialTheme.typography.bodySmall,
-        shape = MaterialTheme.shapes.small,
-        keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.Sentences,
-            imeAction = ImeAction.Done
-        ),
-        minLines = 2,
-        placeholder = {
-            Text(
-                text = "Description",
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
-    )
+	OutlinedTextField(
+		modifier = Modifier.fillMaxWidth(),
+		value = videoDescription(),
+		onValueChange = onVideoDescriptionChange,
+		textStyle = MaterialTheme.typography.bodySmall,
+		shape = MaterialTheme.shapes.small,
+		keyboardOptions = KeyboardOptions(
+			capitalization = KeyboardCapitalization.Sentences,
+			imeAction = ImeAction.Done
+		),
+		minLines = 2,
+		placeholder = {
+			Text(
+				text = "Description",
+				style = MaterialTheme.typography.bodySmall
+			)
+		}
+	)
 }
 
 @Composable
 private fun SelectVideoButton(onClick: () -> Unit) {
-    Button(
-        modifier = Modifier
+	Button(
+		modifier = Modifier
             .heightIn(48.dp)
             .fillMaxWidth(),
-        shape = MaterialTheme.shapes.small,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-            contentColor = MaterialTheme.colorScheme.onSurface
-        ),
-        border = BorderStroke(
-            width = 2.dp,
-            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-        ),
-        onClick = onClick
-    ) {
-        Icon(
-            modifier = Modifier.size(18.dp),
-            imageVector = Icons.Outlined.Videocam,
-            contentDescription = null
-        )
+		shape = MaterialTheme.shapes.small,
+		colors = ButtonDefaults.buttonColors(
+			containerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+			contentColor = MaterialTheme.colorScheme.onSurface
+		),
+		border = BorderStroke(
+			width = 2.dp,
+			color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+		),
+		onClick = onClick
+	) {
+		Icon(
+			modifier = Modifier.size(18.dp),
+			imageVector = Icons.Outlined.Videocam,
+			contentDescription = null
+		)
 
-        Spacer(modifier = Modifier.width(8.dp))
+		Spacer(modifier = Modifier.width(8.dp))
 
-        Text(text = "Select video")
-    }
+		Text(text = "Select video")
+	}
 }
 
 @OptIn(UnstableApi::class)
 @Composable
 private fun SelectedVideo(video: Video) {
-    val context = LocalContext.current
-    val player = remember {
-        val mediaItem = MediaItem.fromUri(video.content)
-        ExoPlayer.Builder(context).build().apply {
-            setMediaItem(mediaItem)
-            prepare()
-        }
-    }
+	val context = LocalContext.current
+	val player = remember {
+		val mediaItem = MediaItem.fromUri(video.content)
+		ExoPlayer.Builder(context).build().apply {
+			setMediaItem(mediaItem)
+			prepare()
+		}
+	}
 
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        border = BorderStroke(
-            width = 2.dp,
-            color = MaterialTheme.colorScheme.onSurface.copy(0.2f)
-        )
-    ) {
-        Column {
-            Box(
-                contentAlignment = Alignment.Center
-            ) {
-                PlayerSurface(
-                    modifier = Modifier
+	Surface(
+		shape = MaterialTheme.shapes.medium,
+		border = BorderStroke(
+			width = 2.dp,
+			color = MaterialTheme.colorScheme.onSurface.copy(0.2f)
+		)
+	) {
+		Column {
+			Box(
+				contentAlignment = Alignment.Center
+			) {
+				PlayerSurface(
+					modifier = Modifier
                         .aspectRatio(ratio = video.size.width.toFloat() / video.size.height.toFloat())
                         .fillMaxWidth(),
-                    player = player
-                )
+					player = player
+				)
 
-                PlayPauseButton(
-                    modifier = Modifier.size(64.dp),
-                    player = player
-                )
-            }
+				PlayPauseButton(
+					modifier = Modifier.size(64.dp),
+					player = player
+				)
+			}
 
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = "${video.size.width} x ${video.size.height}",
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-    }
+			Text(
+				modifier = Modifier.padding(16.dp),
+				text = "${video.size.width} x ${video.size.height}",
+				style = MaterialTheme.typography.bodyMedium
+			)
+		}
+	}
 }
 
 @Composable
 private fun SelectVideoImageButton(onClick: () -> Unit) {
-    Button(
-        modifier = Modifier
+	Button(
+		modifier = Modifier
             .heightIn(48.dp)
             .fillMaxWidth(),
-        shape = MaterialTheme.shapes.small,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-            contentColor = MaterialTheme.colorScheme.onSurface
-        ),
-        border = BorderStroke(
-            width = 2.dp,
-            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-        ),
-        onClick = onClick
-    ) {
-        Icon(
-            modifier = Modifier.size(18.dp),
-            imageVector = Icons.Outlined.Photo,
-            contentDescription = null
-        )
+		shape = MaterialTheme.shapes.small,
+		colors = ButtonDefaults.buttonColors(
+			containerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+			contentColor = MaterialTheme.colorScheme.onSurface
+		),
+		border = BorderStroke(
+			width = 2.dp,
+			color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+		),
+		onClick = onClick
+	) {
+		Icon(
+			modifier = Modifier.size(18.dp),
+			imageVector = Icons.Outlined.Photo,
+			contentDescription = null
+		)
 
-        Spacer(modifier = Modifier.width(8.dp))
+		Spacer(modifier = Modifier.width(8.dp))
 
-        Text(text = "Select image")
-    }
+		Text(text = "Select image")
+	}
 }
 
 @Composable
 private fun SelectedVideoImage(image: Image) {
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        border = BorderStroke(
-            width = 2.dp,
-            color = MaterialTheme.colorScheme.onSurface.copy(0.2f)
-        )
-    ) {
-        Column {
-            AsyncImage(
-                modifier = Modifier.fillMaxWidth(),
-                model = image.content,
-                contentDescription = null,
-                contentScale = ContentScale.Crop
-            )
+	Surface(
+		shape = MaterialTheme.shapes.medium,
+		border = BorderStroke(
+			width = 2.dp,
+			color = MaterialTheme.colorScheme.onSurface.copy(0.2f)
+		)
+	) {
+		Column {
+			AsyncImage(
+				modifier = Modifier.fillMaxWidth(),
+				model = image.content,
+				contentDescription = null,
+				contentScale = ContentScale.Crop
+			)
 
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = "${image.size.width} x ${image.size.height}",
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-    }
+			Text(
+				modifier = Modifier.padding(16.dp),
+				text = "${image.size.width} x ${image.size.height}",
+				style = MaterialTheme.typography.bodyMedium
+			)
+		}
+	}
 }
